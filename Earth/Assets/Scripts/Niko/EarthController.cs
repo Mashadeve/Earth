1467aa73cc2b2d4fdf4f4de5f71ad940aab72210
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-11-16 10:12:03 +0200

    up

--- Earth/Assets/Scripts/Niko/EarthController.cs
+++ Earth/Assets/Scripts/Niko/EarthController.cs
@@ -6,19 +6,17 @@ public class EarthController : MonoBehaviour
 {
     [SerializeField] private ReadInput mouseInput;
     private float horizontalInput, verticalInput;
+    private bool movement;
 
 
     private void Update()
     {
-        if (mouseInput.canDrag == true)
-        {          
+        if (!mouseInput.canDrag == true)
+        {
             horizontalInput = Input.mousePosition.normalized.x;
             verticalInput = Input.mousePosition.normalized.y;
-        } 
-        else if (mouseInput.canDrag == false)
-        {
-            
-        }
+        }      
+        
     }
 
     private void FixedUpdate()
@@ -26,5 +24,4 @@ public class EarthController : MonoBehaviour
         transform.Rotate(Vector3.up * horizontalInput);
         transform.Rotate(Vector3.left * verticalInput);      
     }
-
 }
