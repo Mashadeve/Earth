e1414c9f5be1643bc6ee5b3c46f9a1eb258030f3
Mashadeve <74298578+Mashadeve@users.noreply.github.com>
2021-11-19 14:58:10 +0200

    pls

---
+++ Earth/Assets/Scripts/Niko/Snapper.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Snapper : MonoBehaviour
+{
+    public GameObject target, targetAnchor;
+
+    private void OnMouseDown()
+    {
+        if (target.transform.position == targetAnchor.transform.position)
+        {
+            if (target.GetComponent<Move3D>() == null)
+            {
+                target.AddComponent<Move3D>();
+            }
+        }
+        else if (target.transform.position != targetAnchor.transform.position)
+        {
+            return;
+        }
+    }
+
+    private void OnTriggerEnter(Collider targetAnchor)
+    {
+        target.transform.position = targetAnchor.transform.position;
+        Destroy(target.GetComponent<Move3D>());
+
+        Debug.Log("OSUU");  
+    }    
+
+}
--- Earth/Assets/Scripts/Samu/Move3D.cs
+++ Earth/Assets/Scripts/Samu/Move3D.cs
@@ -6,14 +6,8 @@ public class Move3D : MonoBehaviour
 {
     private Camera mainCamera;
     private float cameraZDistance;
-    //[SerializeField] private GameObject piece;
 
-    [SerializeField] private LayerMask layermask;
-
-    //[SerializeField] private Material defaultMaterial;
-    //[SerializeField] private Texture defaultTexture;
-
-    private GameObject currentObject;
+    private GameObject currentObject, sun, sunAnchor;
 
     private bool mouseOver;
     private bool canMovePieces;
@@ -24,48 +18,12 @@ public class Move3D : MonoBehaviour
         cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
     }
 
-    private void Update()
-    {
-        if (!mouseOver && currentObject != null)
-        {
-            //currentObject.GetComponent<MeshRenderer>().material = defaultMaterial;
-            //currentObject.GetComponent<MeshRenderer>().material.mainTexture = defaultTexture;
-        }
-    }
-
     private void OnMouseDrag()
     {
+        this.gameObject.AddComponent<Move3D>();
         Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
         Vector3 newWorldPosition = mainCamera.ScreenToWorldPoint(screenPosition);
-        
+
         transform.position = new Vector3(newWorldPosition.x, newWorldPosition.y, gameObject.transform.position.z);
     }
-
-    //public void MouseInput()
-    //{
-
-
-    //    mouseOver = false;
-
-    //    if (hitData.collider == null) return;
-
-    //    if (hitData.collider.name == "Seas")
-    //    {
-    //        canDrag = true;
-    //        currentObject = hitData.collider.gameObject;
-    //        currentObject.GetComponent<MeshRenderer>().material = defaultMaterial;
-    //        currentObject.GetComponent<MeshRenderer>().material.mainTexture = defaultTexture;
-    //        mouseOver = true;
-    //    }
-
-    //    mouseOver = true;
-    //    if (mouseOver && hitData.collider.name != "Seas")
-    //    {
-    //        currentObject = hitData.collider.gameObject;
-    //        //currentObject.transform.parent.localPosition = Vector3.zero;
-    //        currentObject.GetComponent<MeshRenderer>().material.color = new Color(0.3f, 0, 0, 0.5f);
-    //        Debug.Log(currentObject);
-
-    //    }
-    //}
 }
