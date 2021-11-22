b83cc6cd2e90023dbf2741f2690d81e68e976d49
Samu <65758544+SmuuSka@users.noreply.github.com>
2021-11-22 09:04:26 +0200

    upd

---
+++ Earth/Assets/Scripts/Samu/PartRaycast.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class PartRaycast : MonoBehaviour
+{
+    private Camera mainCamera;
+    private float cameraZDistance;
+    private void Start()
+    {
+        mainCamera = Camera.main;
+        cameraZDistance = mainCamera.WorldToScreenPoint(transform.position).z;
+    }
+    private void OnMouseDrag()
+    {
+        Vector3 screenPosition = new Vector3(Input.mousePosition.x, Input.mousePosition.y, cameraZDistance);
+        Vector3 newWorldPosition = mainCamera.ScreenToWorldPoint(screenPosition);
+
+        transform.position = new Vector3(newWorldPosition.x, newWorldPosition.y, gameObject.transform.position.z);
+
+
+    }
+
+    private void OnDrawGizmos()
+    {
+        float maxDistance = 5f;
+
+        RaycastHit hit;
+
+        bool isHit = Physics.BoxCast(transform.position, transform.lossyScale / 2, transform.forward ,out hit, transform.rotation, maxDistance);
+        if (isHit)
+        {
+            Gizmos.color = Color.red;
+            Gizmos.DrawRay(transform.position, transform.forward * hit.distance);
+            Gizmos.DrawWireCube(transform.position + transform.forward * hit.distance, transform.lossyScale);
+        }
+
+    }
+}
+
