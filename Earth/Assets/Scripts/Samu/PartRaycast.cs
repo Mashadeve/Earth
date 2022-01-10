df5cdb67492f125e92028d014b6b8c4d6b4e4316
Samu <65758544+SmuuSka@users.noreply.github.com>
2022-01-10 11:23:29 +0200

    Update PartRaycast.cs

--- Earth/Assets/Scripts/Samu/PartRaycast.cs
+++ Earth/Assets/Scripts/Samu/PartRaycast.cs
@@ -62,6 +62,7 @@ public class PartRaycast : MonoBehaviour
 
         var child = transform.GetChild(0).gameObject;
         child.layer = 6;
+
         if (transform.position != GameObject.Find("Earth_10").transform.position)
         {
             transform.position = startPos;
