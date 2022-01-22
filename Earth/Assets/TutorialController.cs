392c7c23284db12e9cbd456cda01601ca42a608e
Samu <65758544+SmuuSka@users.noreply.github.com>
2022-01-22 12:46:22 +0200

    tutorialUpd

--- Earth/Assets/TutorialController.cs
+++ Earth/Assets/TutorialController.cs
@@ -9,35 +9,74 @@ using UnityEngine.SceneManagement;
 public class TutorialController : MonoBehaviour
 {
     [SerializeField] private List<GameObject> tutorialScreen = new List<GameObject>();
+    [SerializeField] private Button playButton;
 
-    private int index;
+    public int index;
 
-    private void Start()
+    private void Awake()
     {
         for (int i = 0; i < tutorialScreen.Count; i++)
         {
             tutorialScreen[i].SetActive(false);
         }
         tutorialScreen[0].SetActive(true);
+        playButton.gameObject.SetActive(false);
     }
 
-    public void NextScreen()
+    private void Start()
     {
-        index++;
+        StartCoroutine(screenTimer());
+    }
+
+    private void NextScreen()
+    {
+        if (index < 3)
+        {
+            index++;
+        }
+        else
+        {
+            index = 0;
+        }
+        
+    }
+
+    private IEnumerator screenTimer()
+    {
+        yield return new WaitForSeconds(5f);
+        NextScreen();
+        StartCoroutine(screenTimer());
     }
 
     private void Update()
     {
-        
-        if (index < tutorialScreen.Count)
+        switch (index)
         {
-            tutorialScreen[index].SetActive(true);
-        }
-        else
-        {
-            SceneManager.LoadScene(2);
+            case 0:
+                tutorialScreen[0].SetActive(true);
+                if (tutorialScreen[3].activeInHierarchy)
+                {
+                    tutorialScreen[3].SetActive(false);
+                }
+                break;
+            case 1:
+                tutorialScreen[0].SetActive(false);
+                tutorialScreen[1].SetActive(true);
+                break;
+            case 2:
+                tutorialScreen[1].SetActive(false);
+                tutorialScreen[2].SetActive(true);
+                break;
+            case 3:
+                tutorialScreen[2].SetActive(false);
+                tutorialScreen[3].SetActive(true);
+                playButton.gameObject.SetActive(true);
+                break;
         }
 
-        
+        if (index == 0)
+        {
+            tutorialScreen[0].SetActive(true);
+        }
     }
 }
